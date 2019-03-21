83598e56c740c4189ba85e72086ea8f394e61011
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-21 11:56:04 +0100

    Set up backend endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const api = require('./api/endpoints');
 
 // cra works in a way, that when webpack runs,
 //it creates a build folder that containes index.html
@@ -12,6 +13,11 @@ const pathToIndexHtml = path.join(pathToBuildFolder, 'index.html');
 // 2 - allow static assests to be serverd off this folder (build)
 
 app.use(express.static(pathToBuildFolder));
+
+// 3 - usually not in real-life projects, but
+// for small portfolio stuff, you can have FE
+// and BE in the same app
+app.use('/api', api);
 
 app.get('/', (req, res) => {
   // 1 - res.sendFile - index.html
