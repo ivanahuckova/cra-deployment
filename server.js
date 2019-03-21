194aab1b14dbb85f553bd2086067e9493c155f54
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-21 18:06:50 +0100

    Create frontend-only boilerplate

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const api = require('./api/endpoints');
-
 // cra works in a way, that when webpack runs,
 //it creates a build folder that containes index.html
 //and bundle.js
@@ -17,7 +15,6 @@ app.use(express.static(pathToBuildFolder));
 // 3 - usually not in real-life projects, but
 // for small portfolio stuff, you can have FE
 // and BE in the same app
-app.use('/api', api);
 
 app.get('/', (req, res) => {
   // 1 - res.sendFile - index.html
