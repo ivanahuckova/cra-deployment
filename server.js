dc607d45f9d02688e5c4b4b0235afc0e504886e4
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-21 11:41:31 +0100

    Initial setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// cra works in a way, that when webpack runs,
+//it creates a build folder that containes index.html
+//and bundle.js
+
+const pathToBuildFolder = path.join(__dirname, 'build');
+const pathToIndexHtml = path.join(pathToBuildFolder, 'index.html');
+
+// 2 - allow static assests to be serverd off this folder (build)
+
+app.use(express.static(pathToBuildFolder));
+
+app.get('/', (req, res) => {
+  // 1 - res.sendFile - index.html
+  res.sendFile(pathToIndexHtml);
+});
+
+app.listen(process.env.PORT || 3000);
